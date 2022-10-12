b2670bd0e4eb39b278bac5bd9785833af55b2f8e
qinko <58616160+qinko233@users.noreply.github.com>
2022-10-12 09:20:45 +0800

    Update consult.js

--- WebSite/AntiPMS/src/api/consult.js
+++ WebSite/AntiPMS/src/api/consult.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 let axios = Axios.create({
-  // baseURL: process.env.NODE_ENV === 'development' ? '/development' : 'http://cloudhospital.knjs.com.cn', // url = base url + request url
+  // baseURL: process.env.NODE_ENV === 'development' ? '/development' : '', // url = base url + request url
   baseURL: 'https://api.qinko.club', // url = base url + request url
   withCredentials: false, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
